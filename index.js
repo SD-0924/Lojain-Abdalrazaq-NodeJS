8dd560f73ef0c9242d39b472bf13063f08ed67ee
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-10-19 22:33:28 +0300

    adding the for loop to count the number of words in each file

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 
+function countWords(content) {
+    // splitting the content by whitespace and filter out empty strings
+    const words = content.trim().split(/\s+/);
+    return words.length;
+}
 // Reading the config file that contains the path of the files
 fs.readFile('config.json', 'utf8', (err, data) => {
     if(err){
@@ -11,6 +16,19 @@ fs.readFile('config.json', 'utf8', (err, data) => {
         const config = JSON.parse(data);
         const filePaths = config.files;
         console.log('Files Paths in the config.JSON file after parsing:', filePaths);
+
+        // Iterating over all the files to count the number of words in each file
+        filePaths.forEach(filePath => {
+            // Read each file asynchronously
+            fs.readFile(filePath, 'utf8', (err, content) => {
+                if (err) {
+                    console.error(`Error reading the file ${filePath}:`, err);
+                    return;
+                }
+                const wordCount = countWords(content);
+                console.log(`${filePath}: ${wordCount} words`);
+            });
+        });
     }
     catch(err){
         console.error('Error parsing the config file:', err);
